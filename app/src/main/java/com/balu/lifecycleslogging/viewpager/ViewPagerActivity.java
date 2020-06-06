5875a941907a5ed0a4c81d464422ca98e7229612
balakrishna <balub513@gmail.com>
2020-06-06 22:57:51 +0530

    added new

--- app/src/main/java/com/balu/lifecycleslogging/viewpager/ViewPagerActivity.java
+++ app/src/main/java/com/balu/lifecycleslogging/viewpager/ViewPagerActivity.java
@@ -3,12 +3,17 @@ package com.balu.lifecycleslogging.viewpager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Menu;
 
 import com.balu.lifecycleslogging.R;
 import com.google.android.material.tabs.TabLayout;
 
 public class ViewPagerActivity extends AppCompatActivity {
+    private static final String TAG = "ViewPagerActivity";
 
     private TabAdapter adapter;
     private TabLayout tabLayout;
@@ -17,6 +22,7 @@ public class ViewPagerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_view_pager);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
@@ -24,7 +30,70 @@ public class ViewPagerActivity extends AppCompatActivity {
         adapter.addFragment(new Tab1Fragment(), "Tab 1");
         adapter.addFragment(new Tab2Fragment(), "Tab 2");
         adapter.addFragment(new Tab3Fragment(), "Tab 3");
+        adapter.addFragment(new Tab4Fragment(), "Tab 4");
+        adapter.addFragment(new Tab5Fragment(), "Tab 5");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart()");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.d(TAG, "onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG, "onNewIntent()");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu()");
+        return super.onCreateOptionsMenu(menu);
     }
 }
